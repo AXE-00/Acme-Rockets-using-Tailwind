449f851f35fb731ad81cfd18b39b020f610668cd
Ashutosh <ashutosh.k.work@gmail.com>
2024-08-04 17:15:41 +0530

    added custom CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'wideScreen': {'raw': '(min-aspect-ratio: 3/2)'},
+        'tallScreen': {'raw': '(min-aspect-ratio: 1/2)'}
+      }
+    },
   },
   plugins: [],
 }
